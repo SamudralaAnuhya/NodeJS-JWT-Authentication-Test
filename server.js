a8f515b63ea5c0ad5860654666084377786ee410
SamudralaAnuhya <156963541+SamudralaAnuhya@users.noreply.github.com>
2024-02-24 18:58:48 -0500

    added changes for Coding exercise-04

---
+++ server.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+const path = require("path");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+// const express = require("express");
+const { expressjwt: exjwt } = require("express-jwt");
+
+const secretKey = process.env.SECRET_KEY || "do not hardcode, only for test";
+
+const jwtmw = exjwt({ secret: secretKey, algorithms: ["HS256"] });
+
+let users = [
+  { id: 1, username: "admin", password: "admin" },
+  { id: 2, username: "itsme", password: "mypass" },
+];
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "Content-type, Authorization");
+  next();
+});
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+
+  for (let user of users) {
+    if (user.username === username && user.password === password) {
+      let token = jwt.sign(
+        { id: user.id, username: user.username },
+        secretKey,
+        {
+          expiresIn: '3m', // code to set  expiration time to 3 minutes
+        }
+      );
+      res.json({
+        message: `Welcome ${user.username}!`,
+        token: token,
+        success: true,
+      });
+      break;
+    } else {
+      res.status(401).json({
+        message: "Invalid Credentials",
+        success: false,
+        err: "Username or password incorrect",
+        token: null,
+      });
+    }
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({
+      success: false,
+      officialError: err,
+      err: "username or password incorrect",
+    });
+  } else {
+    next(err);
+  }
+});
+
+app.get("/api/dashboard", jwtmw, (req, res) => {
+  res.json({
+    success: true,
+    myContent: " this is your private content, it is not visible to others",
+  });
+});
+
+app.get("/api/settings", jwtmw, (req, res) => {
+  res.json({
+    success: true,
+    myContent: "Please use this page to update settings",
+  });
+});
+
+// function verifyToken(req, res, next) {
+//   const token = req.headers("Authorization");
+//   if (!token) return res.status(401).json({ message: "Access denied" });
+
+//   // const decoded = jwt.verify(token, secretKey, err
+// }
